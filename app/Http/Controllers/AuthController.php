9eb11f9b916a7052c6d8e6a0807ae2f973aa5677
fauziardiantama <89296690+fauziardiantama@users.noreply.github.com>
2022-12-20 10:03:33 +0700

    Menambah method controller login dan register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,12 +6,25 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //make method login and register with request
-    public function login()
+    
+    public function indexlogin()
     {
         //return view login
         return view('login');
     }
+    
+    public function indexregister()
+    {
+        //return view login
+        return view('login');
+    }
+    
+    public function login(Request $request)
+    {
+        //return view login
+        return view('login');
+    }  
+        
     public function register(Request $request)
     {
         //return view register
